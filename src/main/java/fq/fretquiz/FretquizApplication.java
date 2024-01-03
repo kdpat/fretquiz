2920309a20bbeb0de0485c429448f4309fd759a8
Kyle Patterson <kylepatterson@omg.lol>
2024-01-02 18:59:28 -0600

    init

---
+++ src/main/java/fq/fretquiz/FretquizApplication.java
@@ -0,0 +1,13 @@
+package fq.fretquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FretquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FretquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fq/fretquiz/FretquizApplicationTests.java
@@ -0,0 +1,13 @@
+package fq.fretquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FretquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
