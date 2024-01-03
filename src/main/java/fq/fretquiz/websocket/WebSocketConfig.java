188843007c6742589118e007d0eba6b1a8701a3f
Kyle Patterson <kylepatterson@omg.lol>
2024-01-02 20:07:47 -0600

    websocket

---
+++ src/main/java/fq/fretquiz/websocket/WebSocketConfig.java
@@ -0,0 +1,96 @@
+package fq.fretquiz.websocket;
+
+import fq.fretquiz.auth.Auth;
+import fq.fretquiz.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.lang.NonNull;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.Objects;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final UserService userService;
+
+    private static final Logger log = LoggerFactory.getLogger(WebSocketConfig.class);
+
+    public WebSocketConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue", "/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .addInterceptors(handshakeInterceptor())
+                .setHandshakeHandler(handshakeHandler());
+    }
+
+    @Bean
+    public HandshakeInterceptor handshakeInterceptor() {
+        return new HandshakeInterceptor() {
+
+            @Override
+            public boolean beforeHandshake(@NonNull ServerHttpRequest request,
+                                           @NonNull ServerHttpResponse response,
+                                           @NonNull WebSocketHandler handler,
+                                           @NonNull Map<String, Object> attrs) {
+                if (request instanceof ServletServerHttpRequest servletReq) {
+                    var cookies = servletReq.getServletRequest().getCookies();
+                    var userToken = Auth.findUserIdToken(cookies).orElseThrow();
+                    attrs.put("userToken", userToken);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void afterHandshake(@NonNull ServerHttpRequest request,
+                                       @NonNull ServerHttpResponse response,
+                                       @NonNull WebSocketHandler handler,
+                                       Exception e) {
+            }
+        };
+    }
+
+    @Bean
+    public DefaultHandshakeHandler handshakeHandler() {
+        return new DefaultHandshakeHandler() {
+
+            @Override
+            protected Principal determineUser(@NonNull ServerHttpRequest request,
+                                              @NonNull WebSocketHandler handler,
+                                              @NonNull Map<String, Object> attrs) {
+                var userToken = (String) attrs.get("userToken");
+                Objects.requireNonNull(userToken);
+
+                var userId = Auth.decodeUserIdToken(userToken).orElseThrow();
+                var user = userService.findUser(userId).orElseThrow();
+
+                return UserPrincipal.from(user);
+            }
+        };
+    }
+}
